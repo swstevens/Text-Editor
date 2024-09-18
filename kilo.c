0d24de37b4c7140e64f58c9d9d9b8ae9a8eeb5fa
Shea Stevens <63370427+swstevens@users.noreply.github.com>
2024-09-18 15:20:18 -0700

    finishing raw mode

--- kilo.c
+++ kilo.c
@@ -1,36 +1,57 @@
+// INCLUDES
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
 
+// DATA
+
 struct termios orig_termios;
 
+//FUNCTIONS
+
+void die(const char*s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    } 
 }
 
 void enableRawMode() {
-    tcgetattr(STDERR_FILENO, &orig_termios);
+    if (tcgetattr(STDERR_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    struct termios raw = orig_termios;
     
-    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_lflag &= ~(OPOST);
+    raw.c_lflag &= ~(CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
+
+// INIT
 
 int main() {
     enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        if (read(STDIN_FILENO, &c, 1) == -1 ) die("read");
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n",c,c);
+            printf("%d ('%c')\r\n",c,c);
         }
+        if (c == 'q') break;
     }
     return 0;
 }
