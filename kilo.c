54463d195343ebc1d37ea8f5cd44b19e5bbaf871
Shea Stevens <63370427+swstevens@users.noreply.github.com>
2024-09-18 15:11:43 -0700

    contrinuing disabling ctrl commands

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <ctype.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <termios.h>
@@ -15,7 +17,7 @@ void enableRawMode() {
     struct termios raw;
     tcgetattr(STDIN_FILENO, &raw);
     
-    raw.c_lflag &= ~(ECHO | ICANON);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -23,6 +25,12 @@ void enableRawMode() {
 int main() {
     enableRawMode();
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n",c,c);
+        }
+    }
     return 0;
 }
