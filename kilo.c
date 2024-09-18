ca743048e225211758b749dfdd0c133a0debf78d
Shea Stevens <63370427+swstevens@users.noreply.github.com>
2024-09-18 13:21:11 -0700

    start text input consumer

--- kilo.c
+++ kilo.c
@@ -1,3 +1,28 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDERR_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
